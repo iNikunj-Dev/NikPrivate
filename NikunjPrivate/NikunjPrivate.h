95ff82750a97d40cf5f9a6ea9aee0f83280047a2
Nikunj <nikunj.iroid@gmail.com>
2022-07-05 23:05:15 +0530

    Initial commit

---
+++ NikunjPrivate/NikunjPrivate.h
@@ -0,0 +1,18 @@
+//
+//  NikunjPrivate.h
+//  NikunjPrivate
+//
+//  Created by Nikunj on 05/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NikunjPrivate.
+FOUNDATION_EXPORT double NikunjPrivateVersionNumber;
+
+//! Project version string for NikunjPrivate.
+FOUNDATION_EXPORT const unsigned char NikunjPrivateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NikunjPrivate/PublicHeader.h>
+
+
